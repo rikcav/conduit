1837e513d571d37f29c68b25ebda83bee4cbb80d
rikcav <henriquearau000@gmail.com>
2024-10-29 20:37:49 -0300

    fix: added data annotation to article

--- java/src/main/java/com/project/conduit/model/Article.java
+++ java/src/main/java/com/project/conduit/model/Article.java
@@ -1,11 +1,13 @@
 package com.project.conduit.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
+@Data
 public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
