14bce67dd555e0e66c41a63beef6480d3b98d9a2
rikcav <henriquearau000@gmail.com>
2024-10-29 20:42:51 -0300

    feat: added user service

---
+++ java/src/main/java/com/project/conduit/service/UserService.java
@@ -0,0 +1,24 @@
+package com.project.conduit.service;
+
+import com.project.conduit.model.User;
+import com.project.conduit.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public User registerUser(User user) {
+        return userRepository.save(user);
+    }
+}
