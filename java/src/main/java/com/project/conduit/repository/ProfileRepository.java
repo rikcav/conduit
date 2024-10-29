f8d0493b9cb07d34de519f09557d0252714199be
rikcav <henriquearau000@gmail.com>
2024-10-29 20:19:24 -0300

    fix: fix profile repository

--- java/src/main/java/com/project/conduit/repository/ProfileRepository.java
+++ java/src/main/java/com/project/conduit/repository/ProfileRepository.java
@@ -1,8 +1,9 @@
 package com.project.conduit.repository;
 
+import com.project.conduit.model.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProfileRepository extends JpaRepository<ProfileRepository, Long> {
+public interface ProfileRepository extends JpaRepository<Profile, Long> {
 }
