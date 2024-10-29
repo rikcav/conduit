5e71072dee07de5d5c56e312552b281b82b2d824
rikcav <henriquearau000@gmail.com>
2024-10-29 20:24:01 -0300

    fix: fixed article repository

--- java/src/main/java/com/project/conduit/repository/ArticleRepository.java
+++ java/src/main/java/com/project/conduit/repository/ArticleRepository.java
@@ -1,7 +1,9 @@
 package com.project.conduit.repository;
 
+import com.project.conduit.model.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ArticleRepository {
+public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
