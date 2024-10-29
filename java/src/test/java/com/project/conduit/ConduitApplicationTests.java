96e7437b00336efef4ff738a9fa76b4ed88f4898
rikcav <henriquearau000@gmail.com>
2024-10-29 15:11:31 -0300

    first commit

---
+++ java/src/test/java/com/project/conduit/ConduitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.conduit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConduitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
