96506189208ce2bd95fb43ab09f2cce95d367d07
rikcav <henriquearau000@gmail.com>
2024-10-29 20:43:02 -0300

    feat: added user controller

---
+++ java/src/main/java/com/project/conduit/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.project.conduit.controller;
+
+import com.project.conduit.model.User;
+import com.project.conduit.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        var savedUser = userService.registerUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
+
+    @GetMapping("/user/{username}")
+    public ResponseEntity<User> findByUsername(@PathVariable String username) {
+        var user = userService.findByUsername(username);
+        return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+}
