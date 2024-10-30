3decf77cda8cff4ffe0aecfb2d00b0eb04c7af2a
rikcav <henriquearau000@gmail.com>
2024-10-30 08:15:31 -0300

    feat: added article controller

---
+++ java/src/main/java/com/project/conduit/controller/ArticleController.java
@@ -0,0 +1,44 @@
+package com.project.conduit.controller;
+
+import com.project.conduit.dto.create.ArticleDTO;
+import com.project.conduit.model.Article;
+import com.project.conduit.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/articles")
+public class ArticleController {
+    private final ArticleService articleService;
+
+    @Autowired
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Article> createArticle(@RequestBody ArticleDTO articleDTO) {
+        var articleCreated = articleService.createArticle(articleDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(articleCreated);
+    }
+
+    @GetMapping("{slug}")
+    public ResponseEntity<Article> findBySlug(@PathVariable String slug) {
+        var articleFound = articleService.findBySlug(slug);
+        return ResponseEntity.status(HttpStatus.OK).body(articleFound);
+    }
+
+    @PutMapping("{slug}")
+    public ResponseEntity<Article> updateArticle(@PathVariable String slug, @RequestBody ArticleDTO articleDTO) {
+        var articleUpdated = articleService.updateArticle(slug, articleDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(articleUpdated);
+    }
+
+    @DeleteMapping("{slug}")
+    public ResponseEntity<Article> deleteArticle(@PathVariable String slug) {
+        articleService.deleteArticle(slug);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
