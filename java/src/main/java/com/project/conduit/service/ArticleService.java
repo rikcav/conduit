89de90a9f4df0cef9e00cfb6a061e9d9f1822b48
rikcav <henriquearau000@gmail.com>
2024-11-07 15:13:12 -0300

    feat: added tag creation upon article creation

--- java/src/main/java/com/project/conduit/service/ArticleService.java
+++ java/src/main/java/com/project/conduit/service/ArticleService.java
@@ -5,8 +5,10 @@ import com.project.conduit.dto.view.ArticleRO;
 import com.project.conduit.dto.view.ArticlesRO;
 import com.project.conduit.exception.ResourceNotFoundException;
 import com.project.conduit.model.Article;
+import com.project.conduit.model.Tag;
 import com.project.conduit.model.User;
 import com.project.conduit.repository.ArticleRepository;
+import com.project.conduit.repository.TagRepository;
 import com.project.conduit.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
@@ -21,19 +23,23 @@ import java.util.List;
 public class ArticleService {
     private final ArticleRepository articleRepository;
     private final UserRepository userRepository;
+    private final TagRepository tagRepository;
 
     @Autowired
-    public ArticleService(ArticleRepository articleRepository, UserRepository userRepository) {
+    public ArticleService(ArticleRepository articleRepository, UserRepository userRepository, TagRepository tagRepository) {
         this.articleRepository = articleRepository;
         this.userRepository = userRepository;
+        this.tagRepository = tagRepository;
     }
 
     public ArticleRO createArticle(ArticleDTO articleDTO) {
         var article = dtoToEntity(articleDTO);
 
         var author = userRepository.findAll().get(0);
-
         article.setAuthor(author);
+
+        getOrCreateTags(articleDTO.article().tagList());
+        article.setTagList(articleDTO.article().tagList());
 
         var savedArticle = articleRepository.save(article);
 
@@ -61,6 +67,9 @@ public class ArticleService {
         var updatedArticle = dtoToEntity(articleDTO);
         updatedArticle.setId(savedArticle.getId());
         updatedArticle.setAuthor(savedArticle.getAuthor());
+
+        getOrCreateTags(articleDTO.article().tagList());
+        updatedArticle.setTagList(articleDTO.article().tagList());
 
         var article = articleRepository.save(updatedArticle);
 
@@ -168,5 +177,20 @@ public class ArticleService {
                 .trim()
                 .replaceAll("\\s+", "-")
                 .toLowerCase();
+    }
+
+    private Tag createTag(String name) {
+        Tag tag = new Tag();
+        tag.setName(name);
+        return tagRepository.save(tag);
+    }
+
+    private Tag findOrCreateTagByName(String name) {
+        return tagRepository.findByName(name)
+                .orElseGet(() -> createTag(name));
+    }
+
+    private void getOrCreateTags(List<String> tagNames) {
+        tagNames.forEach(this::findOrCreateTagByName);
     }
 }
