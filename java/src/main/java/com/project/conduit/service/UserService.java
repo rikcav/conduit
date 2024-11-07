533400038927670c3ee5e17112280879fea1b4b7
rikcav <henriquearau000@gmail.com>
2024-11-07 16:32:53 -0300

    feat: added profile creation upon user creation

--- java/src/main/java/com/project/conduit/service/UserService.java
+++ java/src/main/java/com/project/conduit/service/UserService.java
@@ -2,7 +2,9 @@ package com.project.conduit.service;
 
 import com.project.conduit.dto.create.UserDTO;
 import com.project.conduit.exception.ResourceNotFoundException;
+import com.project.conduit.model.Profile;
 import com.project.conduit.model.User;
+import com.project.conduit.repository.ProfileRepository;
 import com.project.conduit.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,10 +12,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final ProfileRepository profileRepository;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, ProfileRepository profileRepository) {
         this.userRepository = userRepository;
+        this.profileRepository = profileRepository;
     }
 
     public User findByUsername(String username) {
@@ -23,7 +27,13 @@ public class UserService {
 
     public User registerUser(UserDTO userDTO) {
         var user = dtoToEntity(userDTO);
-        return userRepository.save(user);
+
+        var profile = userToProfile(user);
+
+        var savedUser = userRepository.save(user);
+        profileRepository.save(profile);
+
+        return savedUser;
     }
 
     private User dtoToEntity(UserDTO userDTO) {
@@ -34,5 +44,15 @@ public class UserService {
         user.setPassword(userDTO.user().password());
 
         return user;
+    }
+
+    private Profile userToProfile(User user) {
+        Profile profile = new Profile();
+
+        profile.setImage(user.getImage());
+        profile.setBio(user.getBio());
+        profile.setUser(user);
+
+        return profile;
     }
 }
