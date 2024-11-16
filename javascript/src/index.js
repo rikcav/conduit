77fb7bbc45faaef8b9e7db992516ab93a4207ba8
rikcav <henriquearau000@gmail.com>
2024-11-16 11:01:29 -0300

    feat: removed swagger

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const setupSwagger = require("./config/swagger");
 const app = express();
 const routes = require("./routes");
 
@@ -9,9 +8,7 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 routes(app);
-setupSwagger(app);
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
-  console.log("Swagger docs are available on http://localhost:3000/api-docs");
 });
