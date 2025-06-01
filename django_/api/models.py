07fc0d1f570ef3d8cef2c0f08f090255cf73f3f0
rikcav <henriquearau000@gmail.com>
2025-06-01 06:44:57 -0300

    feat(django): tag serializer

--- django_/api/models.py
+++ django_/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class User(models.Model):
     username = models.CharField(max_length=150, unique=True, null=False)
@@ -13,6 +14,7 @@ class User(models.Model):
         Email: {self.email};
         Password: {self.password};
         """
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=64, unique=True)
