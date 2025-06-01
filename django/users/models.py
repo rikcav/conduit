f0fdc298dc9a15f02c08c139b074b0cef0b25e18
rikcav <henriquearau000@gmail.com>
2025-06-01 05:00:54 -0300

    feat: removed django project

--- django/users/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class User(AbstractUser):
-    bio = models.TextField(blank=True, null=True)
-    image = models.URLField(blank=True, null=True)
-
