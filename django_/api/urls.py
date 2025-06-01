2c529cf62d2924490f0bf09a57cfc042a28a9043
rikcav <henriquearau000@gmail.com>
2025-06-01 16:48:58 -0300

    feat(django): user get_all and post

--- django_/api/urls.py
+++ django_/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import tag_detail, tag_list
+from .views import tag_detail, tag_list, user_list
 
 urlpatterns = [
     path("tags", tag_list, name="tag-list"),
     path("tags/<int:pk>", tag_detail, name="tag-detail"),
+
+    path("users", user_list, name="user_list"),
+    # path("users/<int:pk>", user_detail, name="tag-detail"),
 ]
--- django_/api/views.py
+++ django_/api/views.py
@@ -4,10 +4,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import unicodedata
 
-from .models import Tag
-from .serializers import TagSerializer
+from .models import User, Tag
+from .serializers import UserSerializer, TagSerializer
 
 
+# TAGS
 def normalize_string(s):
     # Convert to lowercase, replace spaces with underscores, and remove non-ASCII characters.
     s = s.lower().replace(" ", "_")
@@ -48,3 +49,20 @@ def tag_detail(request, pk):
     # DELETE
     tag.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# USERS
+@api_view(["GET", "POST"])
+def user_list(request):
+    # GET ALL
+    if request.method == "GET":
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # POST
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
