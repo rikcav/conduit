bef10550d73174d415e8bd212f9f281103996980
rikcav <henriquearau000@gmail.com>
2025-06-01 05:56:58 -0300

    feat(django): tag model modified

--- django_/api/models.py
+++ django_/api/models.py
@@ -18,4 +18,7 @@ class Tag(models.Model):
     name = models.CharField(max_length=64, unique=True)
 
     def __str__(self):
-        return self.name
+        return f"""
+        ID: {self.id};
+        Name: {self.name};
+        """
