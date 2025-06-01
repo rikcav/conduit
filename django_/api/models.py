9833b0fb872d4292da573121a8baebd16774f92f
rikcav <henriquearau000@gmail.com>
2025-06-01 05:53:16 -0300

    feat(django): user model

--- django_/api/models.py
+++ django_/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True, null=False)
+    email = models.EmailField(unique=True, null=False)
+    password = models.CharField(max_length=128, null=False)
+
+    def __str__(self):
+        return f"""
+        ID: {self.id};
+        Username: {self.username};
+        Email: {self.email};
+        Password: {self.password};
+        """
