c974cf5e1a27cb7f272322805fa7666ccf70ddd0
rikcav <henriquearau000@gmail.com>
2025-06-07 19:53:13 -0300

    style(express): article update

--- express/src/routes/ArticleRoutes.js
+++ express/src/routes/ArticleRoutes.js
@@ -3,9 +3,9 @@ const controller = require("../controllers/ArticleController");
 module.exports = {
   articleRoutes: (app) => {
     app.get("/api/articles", controller.findAllArticles);
-    app.post('/api/articles', controller.createArticle);
+    app.post("/api/articles", controller.createArticle);
     app.get("/api/articles/:slug", controller.findArticleBySlug);
-    app.put('/api/articles/:slug', controller.updateArticle);
+    app.put("/api/articles/:slug", controller.updateArticle);
     app.delete("/api/articles/:slug", controller.deleteArticle);
   },
 };
