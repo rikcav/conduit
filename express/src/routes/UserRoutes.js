e43447de13a14a50b21cf3610b2dd1bad489063c
rikcav <henriquearau000@gmail.com>
2025-06-07 15:42:16 -0300

    feat(express): user routes

--- express/src/routes/UserRoutes.js
+++ express/src/routes/UserRoutes.js
@@ -2,5 +2,10 @@ const controller = require("../controllers/UserController");
 
 module.exports = {
   userRoutes: (app) => {
+    app.get("/api/users", controller.findAllUsers);
+    app.post("/api/users", controller.createUser);
+    app.get("/api/users/:id", controller.findUserById);
+    app.put("/api/users/:id", controller.updateUser);
+    app.delete("/api/users/:id", controller.deleteUser);
   },
 };
