d300600e4468c843088b6bcc6077ce4225eb4833
rikcav <henriquearau000@gmail.com>
2025-06-07 18:31:16 -0300

    feat(express): article routes added to app routes

--- express/src/routes.js
+++ express/src/routes.js
@@ -1,7 +1,9 @@
 const { userRoutes } = require("./routes/UserRoutes");
 const { tagRoutes } = require("./routes/TagRoutes");
+const { articleRoutes } = require("./routes/ArticleRoutes");
 
 module.exports = (app) => {
   userRoutes(app);
   tagRoutes(app);
+  articleRoutes(app);
 };
