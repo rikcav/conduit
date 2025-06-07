27ab431b4f7e13ca1944995044defa6e129989ae
rikcav <henriquearau000@gmail.com>
2025-06-07 15:52:43 -0300

    feat(express): tag routes added to app routes

--- express/src/routes.js
+++ express/src/routes.js
@@ -1,5 +1,7 @@
 const { userRoutes } = require("./routes/UserRoutes");
+const { tagRoutes } = require("./routes/TagRoutes");
 
 module.exports = (app) => {
   userRoutes(app);
+  tagRoutes(app);
 };
